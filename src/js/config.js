dcb29fef9d2ceb0385de2bf57740e24220c66f2d
Victor LEDEZ <ledezvictor@gmail.com>
2021-02-24 21:25:49 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a3d28ea6-002b-4ac3-b752-7b61585aee8a';
+export const MODAL_CLOSE_SEC = 2.5;
